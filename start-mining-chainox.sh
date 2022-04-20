d8615670999d5cd0b9d03402ee49bfcf8ce4f9c5
lucashsamaral <69067475+lucashsamaral@users.noreply.github.com>
2022-04-20 08:14:42 -0400

    Add files via upload

---
+++ start-mining-chainox.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+export GPU_MAX_HEAP_SIZE=100
+export GPU_MAX_USE_SYNC_OBJECTS=1
+export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_SINGLE_ALLOC_PERCENT=100
+export GPU_ENABLE_LARGE_ALLOCATION=100
+export GPU_MAX_WORKGROUP_SIZE=1024
+
+./SRBMiner-MULTI --disable-cpu --algorithm 0x10 --pool coolminerz.hopto.org:9400 --wallet 5d9JZoEt8mGxhU8to1mHzzjK5rPH2wMVfq --password c=CHOX
---
+++ start-mining-optical-bitcoin.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-cpu --algorithm heavyhash --pool obtc.suprnova.cc:4074 --wallet srbminercom.x --password x
